3c46e2f82c507b3200a128442f948154c712230e
Seokyeong237 <64405757+Seokyeong237@users.noreply.github.com>
2021-09-13 05:56:47 +0900

    과제

---
+++ seokyeong-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React from 'react';
+import Hello from './Hello';
+
+function App() {
+  const name = 'Hwang Seo Kyeong';
+  const style = {
+    backgroundColor: 'black', 
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
+
+  return (
+    <>
+     <Hello />
+     <div style={style}>{name}</div>
+     <div className="gray-box"></div>
+    </>
+  );
+}
+
+export default App;
